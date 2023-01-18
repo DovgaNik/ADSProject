fd9f78d2881450a5caf815f8a9a34847adc2ad96
DovgaNik <nikita.dovgan.05@hotmail.com>
2023-01-18 21:53:46 +0200

    Finished Alternative structures project

--- alternative_structures/alternative_structures/alernative_structures.cpp
+++ alternative_structures/alternative_structures/alernative_structures.cpp
@@ -3,51 +3,50 @@
 
 using namespace std;
 
+void intro() {
+
+	cout << "Project: Alternative strucutues." << endl << "Description: This algorithm will act as a calculator receiving operator and values from comman line interface arguments." << endl << "Made by Nikita DOVHAN dovhan.o.nikita22@stud.rau.ro dovgan.nik.2005@gmail.com" << endl;
+
+}
+
+void finish() {
+
+	cout << "Press enter to finish execution...";
+	cin.get();
+	exit(0);
+
+}
+
 int main(int argc, char** argv) {
 
-	if (argc != 4) {
+	intro();
 
+	if (argc != 4)
 		cout << "Input is invalid, please try again." << endl;
-		return 0;
-
-	}
 	else
-	{
 		if (strcmp(argv[2], "+") == 0)
-		{
-			cout << *argv[1] + *argv[3] << endl;
-		}
+			cout << atof(argv[1]) + atof(argv[3]) << endl;
 		else
-		{
 			if (strcmp(argv[2], "-") == 0)
-			{
-
-			}
+				cout << atof(argv[1]) - atof(argv[3]) << endl;
 			else
-			{
 				if (strcmp(argv[2], "*") == 0)
-				{
-
-				}
+					cout << atof(argv[1]) * atof(argv[3]) << endl;
 				else
-				{
 					if (strcmp(argv[2], "/") == 0)
-					{
-
-					}
+						cout << atof(argv[1]) / atof(argv[3]) << endl;
 					else
-					{
-						if (strcmp(argv[2], "^") == 0)
-						{
+						if (strcmp(argv[2], "^") == 0) {
+
+							double num = atof(argv[1]);
+
+							for (double i = 1; i < atof(argv[3]); i++)
+								num = num * atof(argv[1]);
+
+							cout << num << endl;
 
 						}
-					}
-				}
-			}
-		}
-	}
 	
-	cin.get();
-	return 0;
+	finish();
 
 }
